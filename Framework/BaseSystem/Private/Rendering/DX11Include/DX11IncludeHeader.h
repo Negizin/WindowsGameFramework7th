49598fcd5f64fcc531d08e947ce940c504454bac
anonym3883 <tky.kwkm.3883@gmail.com>
2016-11-08 23:24:31 +0900

    Add [DirextX11 Include Header]

---
+++ Framework/BaseSystem/Private/Rendering/DX11Include/DX11IncludeHeader.h
@@ -0,0 +1,19 @@
+/*! =====================================================================================
+@file		DX11IncludeHeader.h
+@brief		NoClass
+@author		
+@date
+====================================================================================== */
+#pragma once
+
+//インクルード
+#include <D3D11.h>
+#include <d3dcompiler.h>
+#include <DXGI.h>
+
+//使用するライブラリ
+#pragma comment (lib,"d3d11.lib")
+#pragma comment (lib,"d3dcompiler.lib")
+#pragma comment(lib,"DXGI.lib")
+#pragma comment(lib,"dxguid.lib")
+
