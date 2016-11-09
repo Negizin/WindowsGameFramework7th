1d26f1aa14e36fc32d55c37ea09be3c5071de8b6
anonym3883 <tky.kwkm.3883@gmail.com>
2016-11-09 15:14:06 +0900

    add [DepthStencilState]

---
+++ Framework/BaseSystem/Private/RenderSystem/State/DepthStencilState.cpp
@@ -0,0 +1,60 @@
+#include "DepthStencilState.h"
+#include	"../DX11Device/DX11Device.h"
+#include	"../Utilities/ResourceDescription/ResouceDesc.h"
+
+
+DepthStencilState::DepthStencilState()
+	:m_depthTestFunction(COMPARISON_FUNC::LESS),
+	m_ZEnable(true),
+	m_ZWriteEnable(true),
+	m_pDepthStencilState(nullptr) {
+}
+
+DepthStencilState::~DepthStencilState() {
+}
+
+bool DepthStencilState::CreateDepthStencilState(DX11Device* pDev) {
+	DEPTH_STENCIL_DESC desc;
+	desc.DepthWriteMask = (D3D11_DEPTH_WRITE_MASK) m_ZWriteEnable;
+	desc.DepthEnable = m_ZWriteEnable;
+	desc.DepthFunc = (D3D11_COMPARISON_FUNC) m_depthTestFunction;
+
+	HRESULT hr = pDev->GetDevD3D()->CreateDepthStencilState(&desc, &m_pDepthStencilState);
+	if (!SUCCEEDED(hr))
+		return false;
+	return true;
+}
+
+bool DepthStencilState::SetZEnable(bool flag) {
+	m_ZEnable = flag;
+	return true;
+}
+bool DepthStencilState::SetZWriteEnable(bool flag) {
+	m_ZWriteEnable = flag;
+	return true;
+}
+bool DepthStencilState::SetDepthTestFunction(COMPARISON_FUNC func) {
+	m_depthTestFunction = func;
+	return true;
+}
+
+bool DepthStencilState::SetDepthTestFunctionByString(const tstring& str) {
+	if (str == _T("DNEVER"))
+		m_depthTestFunction = COMPARISON_FUNC::NEVER;
+	else if (str == _T("DLESS"))
+		m_depthTestFunction = COMPARISON_FUNC::LESS;
+	else if (str == _T("DEQUAL"))
+		m_depthTestFunction = COMPARISON_FUNC::EQUAL;
+	else if (str == _T("LESS_EQUAL"))
+		m_depthTestFunction = COMPARISON_FUNC::LESS_EQUAL;
+	else if (str == _T("GREATER"))
+		m_depthTestFunction = COMPARISON_FUNC::GREATER;
+	else if (str == _T("NOT_EQUAL"))
+		m_depthTestFunction = COMPARISON_FUNC::NOT_EQUAL;
+	else if (str == _T("GREATER_EQUAL"))
+		m_depthTestFunction = COMPARISON_FUNC::GREATER_EQUAL;
+	else if (str == _T("ALWAYS"))
+		m_depthTestFunction = COMPARISON_FUNC::ALWAYS;
+
+	return true;
+}
---
+++ Framework/BaseSystem/Private/RenderSystem/State/DepthStencilState.h
@@ -0,0 +1,44 @@
+/*!-----------------------------------------------------------------/
+//@brief	DepthStencilState.h
+//
+//  DepthStencilStateLapperClass
+//
+//@Auther   ã{å¥ïqéj
+//@Date     2016/10/31
+//-----------------------------------------------------------------*/
+#pragma once
+#include	<d3d11.h>
+#include	"../../../Public/Common/Common.h"
+
+enum COMPARISON_FUNC {
+	NEVER = 1,			//&0 èÌÇ…ïsçáäi
+	LESS = 2,			//<= É\Å[ÉXÇ™ëŒè€ÇÊÇËè¨Ç≥Ç¢Ç∆Ç´Ç…çáäi
+	EQUAL = 3,			//== ìôÇµÇ¢éûÇ…çáäi
+	LESS_EQUAL = 4,		//<=  è¨Ç≥Ç¢Ç©ìôÇµÇ¢éûÇ…çáäi
+	GREATER = 5,		//>  É\Å[ÉXÇ™ëŒè€ÇÊÇËëÂÇ´Ç¢éûÇ…çáäi
+	NOT_EQUAL = 6,		//!= ìôÇµÇ≠Ç»Ç¢Ç∆Ç´Ç…çáäi
+	GREATER_EQUAL = 7,	//>= ëÂÇ´Ç¢Ç∆Ç´Ç©ìôÇµÇ¢éûÇ…çáäi
+	ALWAYS = 8,			//|1 èÌÇ…çáäi
+};
+
+
+class DX11Device;
+class DepthStencilState {
+public:
+	DepthStencilState();
+	~DepthStencilState();
+	bool CreateDepthStencilState(DX11Device * pDev);
+	bool SetZEnable(bool flag);
+	bool SetZWriteEnable(bool flag);
+	bool SetDepthTestFunction(COMPARISON_FUNC fiunction);
+	bool SetDepthTestFunctionByString(const tstring & name);
+
+	ID3D11DepthStencilState *GetDSS() { return m_pDepthStencilState; }
+private:
+	ID3D11DepthStencilState *m_pDepthStencilState;
+	bool m_ZWriteEnable;
+	bool m_ZEnable;
+	COMPARISON_FUNC m_depthTestFunction;
+
+};
+
