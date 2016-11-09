83b9cb522b3134060a261e358e351e37823c479a
anonym3883 <tky.kwkm.3883@gmail.com>
2016-11-09 14:31:08 +0900

    add,refactoring [RenderFlow]

---
+++ Framework/BaseSystem/Private/RenderSystem/SystemResource/RenderFlow.cpp
@@ -0,0 +1,75 @@
+#include "RenderFlow.h"
+#include "RenderPass.h"
+#include "DrawQueue.h"
+#include "ClearPass.h"
+
+
+bool RenderFlow::Render(DeviceContext * pDc, DrawQueue *Queue) {
+	if (pDc == nullptr || Queue == nullptr)
+		return FALSE;
+
+	for (auto it : m_renderPass) {
+		it->Begin(pDc);
+		it->Render(Queue, pDc);
+		it->End(pDc);
+	}
+	return TRUE;
+}
+
+RenderPass* RenderFlow::GetPipeLine(tstring name) {
+	HASH hash = UT::StringToHash(name);
+
+	for (auto it : m_renderPass) {
+		if (it->GetHashCode() == hash)
+			return it;
+	}
+
+	return nullptr;
+}
+
+
+RenderPass* RenderFlow::GetPipeLine(int index) {
+	if ((unsigned) index > m_renderPass.size())
+		return nullptr;
+	else
+		return m_renderPass[index];
+}
+
+bool RenderFlow::SetPipeLine(RenderPass* pass, int index) {
+	if ((unsigned) index >= m_renderPass.size())
+		m_renderPass.push_back(pass);
+	else
+		m_renderPass[index] = pass;
+
+	return TRUE;
+}
+
+int RenderFlow::GetFlowSize() {
+	return m_renderPass.size();
+}
+
+bool RenderFlow::LoadFlomXML(DX11Device *pDev, const tstring& file) {
+
+	XML xml;
+	xml.LoadXML(UT::Resource::GetShaderFlowDirectory() + file);
+	auto elm = xml.GetRootNode()->GetFirstChild();
+	while (elm != nullptr) {
+		if (elm->GetName() == _T("Pass")) {
+			tstring passName = elm->GetAttribute(_T("Name"))->GetStringValue();
+			RenderPass* pRenderpass;
+			if (passName == _T("Clear")) {
+				pRenderpass = new ClearPass();
+			}
+			else {
+				pRenderpass = new RenderPass();
+			}
+			pRenderpass->SetName(passName);
+			pRenderpass->LoadShaderFromXMLElement(pDev, elm);
+			pRenderpass->LoadIOSlotFromXMLElement(pDev, elm);
+
+			m_renderPass.push_back(pRenderpass);
+		}
+		elm = elm->Next();
+	}
+	return true;
+}
---
+++ Framework/BaseSystem/Private/RenderSystem/SystemResource/RenderFlow.h
@@ -0,0 +1,28 @@
+﻿//!*******************************************************
+//!	RenderFlow.h
+//!
+//!	The class is renderer manage.
+//! Singleton
+#pragma once
+#include <vector>
+#include	"../Utilities/NameObject/NameObject.h"
+#include	"../Utilities/XML/Xml.h"
+
+class DeviceContext;
+class RenderPass;
+class DrawQueue;
+class DX11Device;
+
+class RenderFlow :public NameObject {
+
+public:
+	virtual	bool	Render(DeviceContext * pDc, DrawQueue *Queue);
+	RenderPass*	GetPipeLine(tstring);
+	RenderPass*	GetPipeLine(int index);
+	int	GetFlowSize();
+	bool SetPipeLine(RenderPass * pipeline, int pass = -1);
+	bool LoadFlomXML(DX11Device *pDev, const tstring& file);
+protected:
+	std::vector<RenderPass*> m_renderPass;
+};
+
