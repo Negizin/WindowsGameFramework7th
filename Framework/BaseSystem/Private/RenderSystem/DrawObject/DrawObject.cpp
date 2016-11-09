8c7f78cf82733e2eb68a70e7b3b7750d036c7393
anonym3883 <tky.kwkm.3883@gmail.com>
2016-11-10 08:05:01 +0900

    link bug fix [DrawObject.cpp]

--- Framework/BaseSystem/Private/RenderSystem/DrawObject/DrawObject.cpp
+++ Framework/BaseSystem/Private/RenderSystem/DrawObject/DrawObject.cpp
@@ -1,64 +1,52 @@
 #include	"DrawObject.h"
 #include	"../../../../Utilities/Math/Matrix/Matrix.h"
 
-bool DrawObject::Create(Device* pDev)
-{
-	(void)pDev;
+bool DrawObject::Create(Device* pDev) {
+	(void) pDev;
 	return false;
 }
 
-bool DrawObject::Draw(DeviceContext* context)
-{
-	(void)context;
+bool DrawObject::Draw(DeviceContext* context) {
+	(void) context;
 	return false;
 }
-bool DrawObject::UpdateTransform(DeviceContext* context)
-{
-	(void)context;
+bool DrawObject::UpdateTransform(DeviceContext* context) {
+	(void) context;
 	return false;
 }
-bool DrawObject::UpdateMaterial(DeviceContext* context)
-{
-	(void)context;
+bool DrawObject::UpdateMaterial(DeviceContext* context) {
+	(void) context;
 	return false;
 }
-bool DrawObject::UpdateIndexBuffer(DeviceContext* context)
-{
-	(void)context;
+bool DrawObject::UpdateIndexBuffer(DeviceContext* context) {
+	(void) context;
 	return false;
 }
-bool DrawObject::UpdateVertexBuffer(DeviceContext* context)
-{
-	(void)context;
+bool DrawObject::UpdateVertexBuffer(DeviceContext* context) {
+	(void) context;
 	return false;
 }
-bool DrawObject::ApplyVertexBuffer(DeviceContext* context)
-{
-	(void)context;
+bool DrawObject::ApplyVertexBuffer(DeviceContext* context) {
+	(void) context;
 	return false;
 }
-bool DrawObject::ApplyTransForm(DeviceContext* context)
-{
-	(void)context;
+bool DrawObject::ApplyTransForm(DeviceContext* context) {
+	(void) context;
 	return false;
 }
-bool DrawObject::ApplyIndexBuffer(DeviceContext* context)
-{
-	(void)context;
+bool DrawObject::ApplyIndexBuffer(DeviceContext* context) {
+	(void) context;
 	return false;
 }
-bool DrawObject::ApplyMaterial(DeviceContext* context)
-{
-	(void)context;
+bool DrawObject::ApplyMaterial(DeviceContext* context) {
+	(void) context;
 	return false;
 }
 
-Matrix DrawObject::GetWorldMatrix()
-{
+Matrix DrawObject::GetWorldMatrix() {
 	return Matrix::identity;
 }
 
-Matrix DrawObject::GetLocalMatrix()
-{
+Matrix DrawObject::GetLocalMatrix() {
 	return Matrix::identity;
 }
