b119187b937ac72db21f50f23935a579f86f0d58
anonym3883 <tky.kwkm.3883@gmail.com>
2016-11-09 10:54:32 +0900

    Add,Refactoring [XML.h XML.cpp]

---
+++ Framework/BaseSystem/Private/RenderSystem/Utilities/XML/Xml.cpp
@@ -0,0 +1,390 @@
+#include <sstream>
+#include <functional>
+#include "XML.h"
+namespace XmlUT
+{
+	//========================================================================================
+	// string データから　数値の配列を取得します。
+	//
+	//	型とデータがマッチングしない場合は不正な結果が返ります、
+	//
+	//　例　Int の配列を取得するときに 0.0などの値が含まれていると不正な結果になり得ます
+	//
+	//========================================================================================
+	template<typename Array>
+	static Array ValueToArray4Type(tstring value) {
+		Array data;
+#ifdef UNICODE
+		std::wstringstream	ss(value);
+#else
+		std::stringstream	ss(value);
+#endif // UNICODE
+		ss >> data[0] >>
+			data[1] >>
+			data[2] >>
+			data[3];
+		return data;
+	}
+
+	MyXMLElement* ElementSearchFromChildren(MyXMLElement* elm, tstring name) {
+		if (elm == nullptr)
+			return nullptr;
+
+		if (elm->GetName() == name)
+			return elm;
+		auto child = elm->GetFirstChild();
+		auto subling = elm->Next();
+
+		if (child) {
+			auto res = ElementSearchFromChildren(child, name);
+			if (res)return res;
+		}
+
+		if (subling) {
+			auto res = ElementSearchFromChildren(subling, name);
+			if (res)return res;
+		}
+
+		return (MyXMLElement*)nullptr;
+	};
+}
+
+MyXMLAttribute::MyXMLAttribute() :
+	m_attribute(nullptr) {
+
+};
+
+//==============================================
+//  MyXMLAttribute::GetInt4Value()
+//	値をint4つとして解釈し取得します
+//==============================================
+std::array<int, 4> MyXMLAttribute::GetInt4Value() {
+	if (m_attribute == nullptr)
+		return std::array<int, 4>();
+	return XmlUT::ValueToArray4Type<std::array<int, 4>>(GetStringValue());
+}
+
+//==============================================
+//  MyXMLAttribute::GetInt3Value()
+//	値をint3つとして解釈し取得します
+//==============================================
+std::array<int, 3> MyXMLAttribute::GetInt3Value() {
+	if (m_attribute == nullptr)
+		return std::array<int, 3>();
+	std::array<int, 4> w(XmlUT::ValueToArray4Type<std::array<int, 4>>(GetStringValue()));
+
+	return std::array<int, 3>() = { { w[0] ,w[1],w[2] } };
+}
+
+//==============================================
+//  MyXMLAttribute::GetInt2Value()
+//	値をint2つとして解釈し取得します
+//==============================================
+std::array<int, 2> MyXMLAttribute::GetInt2Value() {
+	if (m_attribute == nullptr)
+		return std::array<int, 2>();
+	std::array<int, 4> w(XmlUT::ValueToArray4Type<std::array<int, 4>>(GetStringValue()));
+
+	return std::array<int, 2>() = { { w[0], w[1] } };
+}
+
+//==============================================
+//  MyXMLAttribute::GetIntValue()
+//	値をintとして解釈し取得します
+//==============================================
+int  MyXMLAttribute::GetIntValue() {
+	if (m_attribute == nullptr)
+		return 0;
+	std::array<int, 4> w(XmlUT::ValueToArray4Type<std::array<int, 4>>(GetStringValue()));
+
+	return w[0];
+}
+
+//==============================================
+//  MyXMLAttribute::GetFloat4Value()
+//	値をFloat4つとして解釈し取得します
+//==============================================
+std::array<float, 4> MyXMLAttribute::GetFloat4Value() {
+	if (m_attribute == nullptr)
+		return std::array<float, 4>();
+	return XmlUT::ValueToArray4Type<std::array<float, 4>>(GetStringValue());
+}
+
+//==============================================
+//  MyXMLAttribute::GetFloat3Value()
+//	値をFloat3つとして解釈し取得します
+//==============================================
+std::array<float, 3> MyXMLAttribute::GetFloat3Value() {
+	if (m_attribute == nullptr)
+		return std::array<float, 3>();
+
+	std::array<float, 4> w(XmlUT::ValueToArray4Type<std::array<float, 4>>(GetStringValue()));
+
+	return std::array<float, 3>() = { {w[0],w[1],w[2]} };
+}
+
+//==============================================
+//  MyXMLAttribute::GetFloat2Value()
+//	値をFloat2つとして解釈し取得します
+//==============================================
+std::array<float, 2> MyXMLAttribute::GetFloat2Value() {
+	if (m_attribute == nullptr)
+		return std::array<float, 2>();
+	std::array<float, 4> w(XmlUT::ValueToArray4Type<std::array<float, 4>>(GetStringValue()));
+
+	return std::array<float, 2>() = { {w[0],w[1]} };
+}
+
+//==============================================
+//  MyXMLAttribute::GetFloatValue()
+//	値をFloatとして解釈し取得します
+//==============================================
+float	 MyXMLAttribute::GetFloatValue() {
+	if (m_attribute == nullptr)
+		return 0;
+
+	std::array<float, 4> w(XmlUT::ValueToArray4Type<std::array<float, 4>>(GetStringValue()));
+
+	return w[0];
+}
+
+//==============================================
+//  MyXMLAttribute::GetStringValue()
+//	値をStringとして解釈し取得します
+//==============================================
+tstring MyXMLAttribute::GetStringValue() {
+	if (m_attribute == nullptr)
+		return _T("nullptr");
+
+#ifdef UNICODE
+	return StringToWString(m_attribute->Value());
+#else
+	return m_attribute->Value();
+#endif	//UNICODE
+}
+
+//==============================================
+//  MyXMLAttribute::GetBoolValue()
+//	値をBoolとして解釈し取得します
+//==============================================
+bool MyXMLAttribute::GetBoolValue() {
+	if (m_attribute == nullptr)
+		return false;
+	return m_attribute->BoolValue();
+}
+
+MyXMLElement::MyXMLElement() :
+	m_element(nullptr),
+	m_firstChild(nullptr),
+	m_parent(nullptr),
+	m_subling(nullptr) {
+	m_attributeList.reserve(32);
+};
+
+MyXMLElement::~MyXMLElement() {
+	SafeDelete(m_subling);
+	SafeDelete(m_firstChild);
+
+	for (auto it : m_attributeList)
+		SafeDelete(it);
+}
+
+
+
+//==============================================
+//  MyXMLElement::GetTextData()
+//	エレメントをテキストとしてすべて取得します
+//==============================================
+tstring MyXMLElement::GetTextData() {
+	if (m_element == nullptr)
+		return _T("nullptr");
+#ifdef UNICODE
+	return StringToWString(m_element->GetText());
+#else
+	return m_element->GetText();
+#endif	//UNICODE
+}
+
+//==============================================
+//  MyXMLElement::GetName()
+//	エレメントの名前を取得します
+//==============================================
+tstring MyXMLElement::GetName() {
+	return m_name;
+}
+
+//==============================================
+//  子エレメントを取得します。
+//	無い場合はnullが返ります。
+//==============================================
+MyXMLElement* MyXMLElement::GetFirstChild() {
+	if (m_element == nullptr)
+		return nullptr;
+	return m_firstChild;
+}
+
+bool MyXMLElement::SetElement(tinyxml2::XMLElement* elm) {
+	m_element = elm;
+
+#ifdef UNICODE
+	m_name = StringToWString(elm->Name());
+	m_data = StringToWString(elm->GetText()) != _T("nullptr") ? StringToWString(elm->GetText()) : _T("");
+#else
+	m_name = elm->Name();
+	m_data = elm->GetText() != nullptr ? elm->GetText() : "";
+#endif //UNICODE
+
+	auto attribute = m_element->FirstAttribute();
+
+	while (attribute != nullptr) {
+		MyXMLAttribute* a = new MyXMLAttribute;
+		a->m_attribute = (tinyxml2::XMLAttribute*)attribute;
+		m_attributeList.push_back(a);
+		attribute = attribute->Next();
+	}
+
+	return true;
+}
+
+//==============================================
+//  子エレメントを名前で取得
+//==============================================
+MyXMLElement* MyXMLElement::FindChild(const tstring& name) {
+	return XmlUT::ElementSearchFromChildren(m_firstChild, name);
+}
+
+MyXMLElement* MyXMLElement::FindChildForAttribute(const tstring& attribute, tstring value) {
+
+	std::function<MyXMLElement*(MyXMLElement*, tstring)>  Search = [&](MyXMLElement* elm, tstring name) {
+		auto at = elm->GetAttribute(attribute);
+
+		if (at != nullptr && at->GetStringValue() == name) {
+			return elm;
+		}
+		if (at) {
+			tstring atc;
+			atc = at->GetStringValue();
+		}
+
+		auto child = elm->GetFirstChild();
+		auto subling = elm->Next();
+
+		if (child) {
+			auto res = Search(child, name);
+			if (res)return res;
+		}
+
+		if (subling) {
+			auto res = Search(subling, name);
+			if (res)return res;
+		}
+
+		return (MyXMLElement*)nullptr;
+	};
+
+	return Search(this, value);
+}
+
+//==============================================
+//  attributeを取得
+//==============================================
+MyXMLAttribute* MyXMLElement::GetAttribute(const tstring& name) {
+	for each (auto var in m_attributeList) {
+#ifdef UNICODE
+		if (name == StringToWString(var->m_attribute->Name()))
+			return var;
+#else
+		if (name == var->m_attribute->Name())
+			return var;
+#endif
+	}
+	return nullptr;
+}
+
+
+//==============================================
+//  同階層の次のエレメントを取得します。
+//	無い場合はnullが返ります。
+//==============================================
+MyXMLElement* MyXMLElement::Next() {
+	return m_subling;
+}
+
+//==============================================
+//  MyXMLElement::TsXML()
+//	constructor
+//==============================================
+XML::XML()
+	:m_rootNode(nullptr) {
+
+};
+
+XML::XML(const tstring& filename) {
+	XML::XML();
+	LoadXML(filename);
+}
+
+bool XML::LoadXML(const tstring& filename) {
+
+#ifdef UNICODE
+	if (m_docment.LoadFile(WStringToString(filename.c_str()).c_str()) != tinyxml2::XMLError::XML_SUCCESS)
+		return false;
+#else
+	if (m_docment.LoadFile(filename.c_str()) != tinyxml2::XMLError::XML_SUCCESS)
+		return false;
+#endif
+
+	m_rootNode = new MyXMLElement;
+	m_rootNode->SetElement(m_docment.RootElement());
+
+	std::function<void(MyXMLElement*)> CreateTree = [&](MyXMLElement* tselm) {
+		if (tselm == nullptr || tselm->m_element == nullptr)
+			return;
+
+		tinyxml2::XMLElement* child = tselm->m_element->FirstChildElement();
+		if (child != nullptr) {
+			tselm->m_firstChild = new MyXMLElement;
+			tselm->m_firstChild->SetElement(child);
+			tselm->m_firstChild->m_parent = tselm;
+			CreateTree(tselm->m_firstChild);
+		}
+
+		tinyxml2::XMLElement* sub = tselm->m_element->NextSiblingElement();
+		if (sub != nullptr) {
+			tselm->m_subling = new MyXMLElement;
+			tselm->m_subling->SetElement(sub);
+			CreateTree(tselm->m_subling);
+		}
+	};
+	CreateTree(m_rootNode);
+
+	return true;
+}
+
+XML::~XML() {
+	SafeDelete(m_rootNode);
+}
+
+MyXMLElement* XML::GetRootNode() {
+	return m_rootNode;
+}
+
+MyXMLElement* XML::FindFirst(const tstring& name) {
+	return XmlUT::ElementSearchFromChildren(m_rootNode, name);
+}
+
+bool XML::SaveXml(const tstring& filename) {
+
+#ifdef UNICODE
+	if (m_docment.SaveFile(WStringToString(filename.c_str()).c_str(), true) != tinyxml2::XMLError::XML_SUCCESS) {
+		//DebugLog("%s.xmlファイルの書き出しに失敗\n", filename.c_str());
+		return false;
+	}
+#else
+	if (m_docment.SaveFile(filename.c_str(), true) != tinyxml2::XMLError::XML_SUCCESS) {
+		//DebugLog("%s.xmlファイルの書き出しに失敗\n", filename.c_str());
+		return false;
+	}
+#endif
+	return true;
+}
---
+++ Framework/BaseSystem/Private/RenderSystem/Utilities/XML/Xml.h
@@ -0,0 +1,116 @@
+/*! =====================================================================================
+@file		Xml.h
+@brief		XmlPerserのLapper
+@author
+@date		2016.11.09	最終更新
+====================================================================================== */
+#pragma once
+#include	"../../../../Public/Common/Common.h"
+#include	 "tiny\tinyxml2.h"
+#include	 <string>
+#include	 <array>
+#include	 <vector>
+
+//==================================================
+//! xml アトリビュート
+//==================================================
+class MyXMLAttribute {
+	friend class MyXMLElement;
+public:
+	MyXMLAttribute();
+	//==================================================
+	//	テキストデータを適正なデータに変換し取得します。
+	//==================================================
+	std::array<int, 4> GetInt4Value();
+	std::array<int, 3> GetInt3Value();
+	std::array<int, 2> GetInt2Value();
+	int  GetIntValue();
+
+	std::array<float, 4> GetFloat4Value();
+	std::array<float, 3> GetFloat3Value();
+	std::array<float, 2> GetFloat2Value();
+	float	 GetFloatValue();
+	bool	 GetBoolValue();
+
+	tstring GetStringValue();
+private:
+	tinyxml2::XMLAttribute* m_attribute;
+};
+
+//==================================================
+//! xml エレメント
+//==================================================
+class MyXMLElement {
+	friend class XML;
+public:
+
+	//! コンストラクタ
+	MyXMLElement();
+	//! デストラクタ
+	virtual ~MyXMLElement();
+
+	//! テキストブロックを取得
+	tstring	  GetTextData();
+	//! エレメントの名前を取得
+	tstring	  GetName();
+
+	//! 次のエレメントを取得
+	MyXMLElement* Next();
+
+	//! 第一子のエレメントを取得
+	MyXMLElement* GetFirstChild();
+
+	//! エレメントを名前で検索し、取得する
+	MyXMLElement* FindChild(const tstring& name);
+
+	//! 子エレメントを検索する
+	//  指定されたアトリュビュート名を持つエレメントが存在しそのデータ要素がvalueである
+	//	子エレメントを取得する
+	MyXMLElement* FindChildForAttribute(const tstring& attributeName, tstring value);
+
+	//! 指定された名前を持つアトリビュートを取得する
+	MyXMLAttribute* GetAttribute(const tstring& name);
+private:
+	tstring m_name;							//! エレメントの名前
+	tstring m_data;							//! エレメントが所持するテキストデータ
+	std::vector<MyXMLAttribute*> m_attributeList;	//! アトリビュートのリスト
+
+	MyXMLElement*		  m_parent;				//! 親エレメント
+	MyXMLElement*		  m_subling;			//! 兄弟エレメント
+	MyXMLElement*		  m_firstChild;			//! 第一子エレメント
+
+	tinyxml2::XMLElement* m_element;			//! tinyxml2の本体
+private:
+	bool SetElement(tinyxml2::XMLElement*);	//! tinyxml2のデータを設定する
+};
+
+//==================================================
+//! xml ファイルデータ
+//==================================================
+class XML {
+	friend class MyXMLElement;
+public:
+	//! コンストラクタ
+	XML();
+	XML(const tstring&  filename);
+
+	//デストラクタ
+	virtual ~XML();
+
+	//! XMLを読み込む
+	bool LoadXML(const tstring& filename);
+
+	//! ルートのエレメントを取得する
+	MyXMLElement* GetRootNode();
+
+	//! 名前で検索し最初に見つかったエレメントを取得する
+	MyXMLElement* FindFirst(const tstring& name);
+
+	//! XMLデータを保存する。
+	//  todo 未実装
+	bool SaveXml(const tstring& filename);
+
+private:
+	tinyxml2::XMLDocument m_docment;	// tinyxml2の本体
+	MyXMLElement* m_rootNode;			// ルートエレメント
+};
