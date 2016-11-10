a867220ca8ee788bf82efaaf54fce9733f0d12ab
anonym3883 <tky.kwkm.3883@gmail.com>
2016-11-10 15:58:04 +0900

    update [Transform]

--- Framework/ComponentClasses/Transform/Transform.h
+++ Framework/ComponentClasses/Transform/Transform.h
@@ -2,7 +2,7 @@
 @file		Transform.h
 @brief		オブジェクトの拡縮・回転・移動を管理するコンポーネント
 @author		Takuya Kawakami
-@date		2016.11.05	新規作成
+@date		2016.11.10　最終更新
 ====================================================================================== */
 #pragma once
 #include	"../Component.h"
@@ -19,12 +19,19 @@ public:
 	virtual ~Transform();	
 
 	//
-	const Vector3&		GetLocalScale()const;
-	const Quaternion&	GetLocalRotation()const;
-	const Vector3&		GetLocalPosition()const;
-	const tstring&		GetName()const;
-	const GameObject*	GetGameObject()const;
-	const Transform*	GetParentTransform()const;
+	void	Teleport(const Vector3& _position);
+	void	Translate(const Vector3& _vector);
+	void	LookDirection(const Vector3& _direction);
+	void	LookAt(const Vector3& _point);
+	void	Resize(const Vector3& _scale);
+	void	Scaling(const Vector3& _parameter);
+	const	Vector3&	GetLocalScale()const;
+	const	Quaternion&	GetLocalRotation()const;
+	const	Vector3&	GetLocalPosition()const;
+	//
+	const	tstring&	GetName()const;
+	const	GameObject*	GetGameObject()const;
+	const	Transform*	GetParentTransform()const;
 	//
 	Matrix	ToWorldMatrix()const;
 	Matrix	ToLocalMatrix()const;
