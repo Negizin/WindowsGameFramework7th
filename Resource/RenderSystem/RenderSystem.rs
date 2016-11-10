67d92025cc6f9199b67e885136baf301078b47c1
anonym3883 <tky.kwkm.3883@gmail.com>
2016-11-11 02:53:28 +0900

    add Resource Directory

---
+++ Resource/RenderSystem/RenderSystem.rs
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+
+<RenderSystem>
+  <Resource resource ="shaderResource.shaderResource"/>
+  <PreRender flow=""/>
+  <Render flow="mainflow.shaderflow"/>
+  <PostRender flow=""/>
+</RenderSystem>
