675c2f8805229100ad4b40ea020569b859e56bde
anonym3883 <tky.kwkm.3883@gmail.com>
2016-11-11 12:17:42 +0900

    add [DemoScene]

---
+++ DemoScene/DemoScene.h
@@ -0,0 +1,27 @@
+	/*! =====================================================================================
+@file		DemoScene.h
+@brief		デモシーン
+@author		Takuya Kawakami
+@date		2016.11.11	新規作成
+====================================================================================== */
+#pragma once
+#include	"../Framework/BaseSystem/Public/Scene/BaseScene.h"
+
+class Camera;
+
+
+class DemoScene :public BaseScene {
+public:
+	DemoScene();
+	~DemoScene();
+
+	void		Initialize();
+	BaseScene*	Update();
+	void		UpdateRenderer();
+
+private:
+	Camera*	m_pCamera;
+
+
+};
+
