5ff74f8b1099c4e59775d5ed9f75bc76b621a23c
anonym3883 <tky.kwkm.3883@gmail.com>
2016-11-14 11:27:35 +0900

    update [DemoSxene]

--- DemoScene/DemoScene.cpp
+++ DemoScene/DemoScene.cpp
@@ -1,5 +1,11 @@
 #include	"DemoScene.h"
+
+#include	"../Framework/BaseSystem/Private/RenderSystem/RenderSystem.h"
+#include	"../Framework/BaseSystem/Private/RenderSystem/ResourceManager/ResourceManager.h"
 #include	"../Framework/GameObjectClasses/Camera/Camera.h"
+#include	"../Framework/BaseSystem/Private/RenderSystem/Mesh/MeshFactory.h"
+#include	"../Framework/BaseSystem/Private/RenderSystem/Mesh/MeshObject.h"
+#include	"../Framework/BaseSystem/Private/RenderSystem/SystemResource/DrawQueue.h"
 
 /*! =====================================================================================
 @brief	コンストラクタ
@@ -22,17 +28,37 @@ DemoScene::~DemoScene() {
 ====================================================================================== */
 void DemoScene::Initialize() {
 
-	
-
-
 	m_pCamera = new Camera();
+	m_pCamera->CreateCBuffer();
 	m_objectManager.RegisterObject(m_pCamera);
 
+	ResourceManager::Initialize();
+	RenderSystem::LoadRenderSystemFromXML(_T("RenderSystem.rs"));
+
+	MeshFactory	_meshFactory;
+	_meshFactory.LoadModelFromFile(_T("Resource/Model/miku/Tda式初音ミク・アペンド_Ver1.00_ボーン未改造.pmx"), _T("miku"));
+
+	MeshObject*	_pMesh = ResourceManager::Find<MeshObject>(_T("miku"));
+
+	for (int i = 0; i < _pMesh->GetGeometryCount(); i++) {
+		m_pQueue->Add(_pMesh->GetGeometry(i));
+	}
+
+	RenderSystem::SetDrawQue(m_pQueue);
 
 }
 
+/*! =====================================================================================
+@brief	更新処理
+@param	void
+@return void
+====================================================================================== */
 BaseScene * DemoScene::Update() {
-	return nullptr;
+	
+	//全ｵﾌﾞｼﾞｪｸﾄの更新処理
+	m_objectManager.Update();
+
+	return this;
 }
 
 /*! =====================================================================================
--- DemoScene/DemoScene.h
+++ DemoScene/DemoScene.h
@@ -17,7 +17,6 @@ public:
 
 	void		Initialize();
 	BaseScene*	Update();
-	void		UpdateRenderer();
 
 private:
 	Camera*	m_pCamera;
