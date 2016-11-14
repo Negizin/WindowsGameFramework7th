8255e7a1530a1eb0567b5649b5a5bd6e4c0ac8e6
anonym3883 <tky.kwkm.3883@gmail.com>
2016-11-14 17:05:45 +0900

    update [Collider]

--- Framework/ComponentClasses/Collider/Collider.h
+++ Framework/ComponentClasses/Collider/Collider.h
@@ -6,13 +6,61 @@
 ====================================================================================== */
 #pragma once
 #include	"../Component.h"
+#include	"../../GameProperty/ColliderID.h"
+#include	"../../Utilities/Math/Vector/Vector.h"
+#include	<list>
 
-class Collider :public	Component{
-public:
-	Collider();
-	virtual ~Collider();
-
-private:
-
+/*! =====================================================================================
+@brief	衝突情報 格納用 構造体
+====================================================================================== */
+struct CollisionData {
+	ColliderIdentifier	opponentID;	//衝突対象のID
+	Vector3	crossPoint;		//衝突点
+	Vector3 normalVector;	//衝突点における法線ベクトル
 };
 
+
+
+/*! =====================================================================================
+@brief	コライダーコンポーネントクラス
+====================================================================================== */
+class Collider :public	Component {
+public:
+	Collider(GameObject* const _pParent);
+	virtual ~Collider();
+
+	void	Initialize(const ColliderIdentifier& _collisionMask);
+
+	const	ColliderIdentifier&	GetColliderID()const;
+	const	CollisionData&	GetCollisionData()const;
+
+private:
+	ColliderIdentifier	m_colliderID;		//自身の衝突ID
+	ColliderIdentifier	m_collisionMask;	//衝突対象IDのマスク
+	CollisionData		m_collisionData;	//衝突情報 格納用
+};
+
+
+
+/*! =====================================================================================
+@brief	コライダーコンポーネントリストのラッピングクラス（std::list<Collider*>）
+====================================================================================== */
+class ColliderList :public	std::list<Collider*> {
+public:
+	ColliderList();
+	~ColliderList();
+
+	void	Activate(bool _flg);
+};
+
+
+
+/*! =====================================================================================
+@brief	衝突タイプの列挙
+====================================================================================== */
+enum ColliderType {
+	NOT_INITIALIZED,	//非初期化状態
+	AABB,				//AxisAlignedBoundingBox：軸平行境界ボックス
+	OBB,				//OrientedBoundingBox:指向性境界ボックス
+	SPHERE,				//球
+};
